2829f86ad424b4f95be9fb6d13b729acaf0d30f8
dickscs <dickscs@gmail.com>
2017-04-15 17:10:40 +0800

    update20170415

--- www/js/kwikinit.js
+++ www/js/kwikinit.js
@@ -38,6 +38,24 @@
 		return k?p[k]:p;
 	}
 
+	// Mathemetic Degree to Radian 
+	deg2rad = function(degree) {
+		return degree * Math.PI / 180;
+	}
+	
+	// Calculate Distance(km) between 2 points in latitude and longitude
+	function LatLng2distance(lat1, lat2, lng1, lng2) {
+		var R = 6371; // Radius of the earth in km
+		
+		var dLat = deg2rad(lat2-lat1);  // deg2rad below
+		var dLng = deg2rad(lng2-lng1); 
+		
+		var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLng/2) * Math.sin(dLng/2); 
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+		var d = R * c; // Distance in km
+		return d;
+	}
+
 	
 	// Initialize Android Back button 
        document.addEventListener("backbutton", function (e) {
